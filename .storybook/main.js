59f667b908da382fec8c114c85a950ebacda60b2
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-07-26 11:56:44 -0500

    🏗️ Get stories from any workspace sub-package.

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,15 +3,9 @@ module.exports = {
     // @workspace sui-stories
     "../sui-stories/**/*.stories.mdx",
     "../sui-stories/**/*.stories.@(js|jsx|ts|tsx)",
-    // @workspace sui-icons
-    "../sui-icons/**/*.stories.mdx",
-    "../sui-icons/**/*.stories.@(js|jsx|ts|tsx)",
-    // @workspace sui-css
-    "../sui-css/packages/**!(/__tests__)/*.stories.mdx",
-    "../sui-css/packages/**/*.stories.@(js|jsx|ts|tsx)",
-    // @workspace sui-react
-    "../sui-react/packages/**/*.stories.mdx",
-    "../sui-react/packages/**/*.stories.@(js|jsx|ts|tsx)",
+    // @workspace sub-packages
+    "../**/sui-stories/*.stories.mdx",
+    "../**/sui-stories/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
