f47030f9819aa8a22c18a9afacd4eb860170af15
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-07-27 20:20:14 -0500

    🏗️ Migrate Storybook to Webpack 5 support.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,19 @@
 module.exports = {
-  "stories": [
-    // @workspace sui-stories
-    "../sui-stories/**/*.stories.mdx",
-    "../sui-stories/**/*.stories.@(js|jsx|ts|tsx)",
-    // @workspace sub-packages
-    "../**/sui-stories/*.stories.mdx",
-    "../**/sui-stories/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
+	stories: [
+		// @workspace sui-stories
+		"../sui-stories/**/*.stories.mdx",
+		"../sui-stories/**/*.stories.@(js|jsx|ts|tsx)",
+		// @workspace sub-packages
+		"../**/sui-stories/*.stories.mdx",
+		"../**/sui-stories/*.stories.@(js|jsx|ts|tsx)",
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions"
+	],
+	framework: "@storybook/react",
+	core: {
+		"builder": "@storybook/builder-webpack5"
+	}
 }
