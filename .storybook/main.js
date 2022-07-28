faa874f453b67df988cd8f7f80612f7572372cae
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-07-27 22:29:23 -0500

    🏗️ Storybook SASS support.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
 	stories: [
 		// @workspace sui-stories
@@ -10,8 +12,23 @@ module.exports = {
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
-		"@storybook/addon-interactions"
+		"@storybook/addon-interactions",
+		"@storybook/addon-postcss"
 	],
+	webpackFinal: async (config, { configType }) => {
+		// Add SASS support.
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: [
+				'style-loader',
+				'css-loader',
+				'sass-loader'
+			],
+			include: path.resolve( __dirname, '../' ),
+		});
+
+		return config;
+	},
 	framework: "@storybook/react",
 	core: {
 		"builder": "@storybook/builder-webpack5"
