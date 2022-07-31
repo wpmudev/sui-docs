b41905d5951429ac27977ad934fb6a1ee307dfc4
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-07-31 16:43:57 -0500

    🏗️ Relocate docs packages.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,6 @@ const path = require('path');
 
 module.exports = {
 	stories: [
-		// @workspace sui-stories
-		"../sui-stories/**/*.stories.mdx",
-		"../sui-stories/**/*.stories.@(js|jsx|ts|tsx)",
-		// @workspace sub-packages
 		"../**/sui-stories/*.stories.mdx",
 		"../**/sui-stories/*.stories.@(js|jsx|ts|tsx)",
 	],
