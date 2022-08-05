c7cf04158533e76079d5bde12388db590d3e834c
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-08-04 22:38:36 -0500

    ✨ new(elements): Table for documentation.

--- packages/docs-elements/lib/docs-elements.js
+++ packages/docs-elements/lib/docs-elements.js
@@ -2,6 +2,7 @@ import { Banner } from './elements/banner';
 import { Page } from './elements/page';
 import { Content } from './elements/content';
 import { Section } from './elements/section';
+import { Table, TableHead, TableBody } from './elements/table';
 import { Footer } from './elements/footer';
 
 export const Docs = {};
@@ -10,4 +11,7 @@ Docs.Banner = Banner;
 Docs.Page = Page;
 Docs.Content = Content;
 Docs.Section = Section;
+Docs.Table = Table;
+Docs.TableHead = TableHead;
+Docs.TableBody = TableBody;
 Docs.Footer = Footer;
