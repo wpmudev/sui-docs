7ad8f89d749f6ce73e86d7dc9477449908016de4
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-08-06 18:13:39 -0500

    🐛 fix(elements): Add status tag next to page title.

--- packages/docs-elements/lib/elements/page.js
+++ packages/docs-elements/lib/elements/page.js
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../assets/css/page.css';
+import '../assets/css/tooltip.css';
 
-export const Page = ({ title, light = false, children }) => {
+export const Page = ({ title, status, light = false, children }) => {
+	const [ isHovering, setIsHovering ] = useState( false );
+
+	const handleMouseOver = () => {
+		setIsHovering( true );
+	}
+
+	const handleMouseOut = () => {
+		setIsHovering( false );
+	}
+
+	let statusName, statusDesc, statusClass;
+
+	switch ( status ) {
+		case 'planned':
+			statusName = 'Planned';
+			statusDesc = 'Planned, not ready for use';
+			statusClass = '';
+			break;
+
+		case 'draft':
+			statusName = 'Draft';
+			statusDesc = 'Draft, still in development mode';
+			statusClass = ' csb-tag--yellow';
+			break;
+
+		case 'ready':
+			statusName = 'Ready';
+			statusDesc = 'Ready, can be used in production'
+			statusClass = ' csb-tag--blue';
+			break;
+
+		case 'dead':
+			statusName = 'Deprecated';
+			statusDesc = 'Deprecated, soon to be removed';
+			statusClass = ' csb-tag--red';
+			break;
+
+		default:
+			statusName = false;
+			statusClass = '';
+			break;
+	}
+
+	const statusTag = (
+		<span className={`csb-tooltip${ isHovering ? ' csb-tooltip--active' : '' }`}>
+			<span
+				className={`csb-tag${ statusClass }`}
+				aria-describedby={`page-status--${ status && status !== '' ? status : 'planned' }`}
+				onMouseOver={ handleMouseOver }
+				onMouseOut={ handleMouseOut }>
+				{ statusName }
+			</span>
+			<span role="tooltip" className="csb-tooltip__message">
+				{ isHovering && (
+					<>{ statusDesc }</>
+				)}
+			</span>
+		</span>
+	);
+
 	return (
 		<div className={ `csb-page${ light ? ' csb-page--white' : '' }` }>
 			{ '' !== title &&
 				<div className="csb-heading">
 					<div className="csb-content">
-						<h1 className="csb-page__title">{ title }</h1>
+						<h1 className="csb-page__title">{ title }{ statusName ? statusTag : '' }</h1>
 					</div>
 				</div>
 			}
@@ -17,3 +78,5 @@ export const Page = ({ title, light = false, children }) => {
 		</div>
 	);
 }
+
+// <span id="tip1" class="tooltip" role="tooltip" >Also known as User ID</span>
