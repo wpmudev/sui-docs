4acaeea045b05e677c557a5caed6975915c9eefc
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-10-10 22:00:15 -0500

    🐛 fix(button): Update markup.

--- packages/docs-button/lib/docs-button.js
+++ packages/docs-button/lib/docs-button.js
@@ -1,7 +1,37 @@
-'use strict';
+import React from "react";
 
-module.exports = docsButton;
+// Build "button" component.
+const Button = ({ label, icon, ...args }) => {
+	return (
+		<button className="csb-button" { ...args }>
+			{ !isUndefined( icon ) && (
+				<span className="csb-button__icon">
+					<span className={`csb-icon csb-icon--${ icon }`} />
+				</span>
+			)}
 
-function docsButton() {
-    // TODO
+			<span className="csb-button__label">{ label }</span>
+		</button>
+	);
 }
+
+// Check if element is undefined.
+const isUndefined = (element, isNumber = false) => {
+	const isValid = 'undefined' !== typeof element;
+	const isNotEmpty = '' !== element;
+
+	if ( element && isValid && isNotEmpty ) {
+		if ( isNumber ) {
+			if ( Number.isNaN(element) ) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+// Publish required component(s).
+export { Button }
