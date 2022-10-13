0e8c2f39c2eccd5a51ea27fb51054096715f6f84
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-10-13 13:44:08 -0500

    🎨 fix(button): Improve markup and styles.

--- packages/docs-button/lib/docs-button.js
+++ packages/docs-button/lib/docs-button.js
@@ -1,16 +1,42 @@
 import React from "react";
 
+// Import required styles.
+import "./docs-button.scss";
+
 // Build "button" component.
-const Button = ({ label, icon, ...args }) => {
+const Button = ({ label, icon, style, color, className, ...args }) => {
+	let btnClass = 'csb-button';
+
+	switch( style ) {
+		case 'primary':
+		case 'secondary':
+			btnClass += ' ' + 'csb-button--' + style;
+			break;
+
+		default:
+			// Do nothing.
+			break;
+	}
+
+	if ( !isUndefined( color ) ) {
+		btnClass += ' ' + 'csb-button--' + color;
+	}
+
+	if ( !isUndefined( className ) ) {
+		btnClass += ' ' + className;
+	}
+
 	return (
-		<button className="csb-button" { ...args }>
+		<button className={ btnClass } { ...args }>
 			{ !isUndefined( icon ) && (
 				<span className="csb-button__icon">
 					<span className={`csb-icon csb-icon--${ icon }`} />
 				</span>
 			)}
 
-			<span className="csb-button__label">{ label }</span>
+			<span className="csb-button__label">
+				{ label }
+			</span>
 		</button>
 	);
 }
