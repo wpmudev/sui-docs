a70923152784cbafead74ebd1df3a2ca0b8f7826
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-10-13 13:44:19 -0500

    🎨 fix(card): Improve markup and styles.

--- packages/docs-card/lib/docs-card.js
+++ packages/docs-card/lib/docs-card.js
@@ -1,16 +1,71 @@
 import React from "react";
+import { Button } from "@wpmudev/docs-button";
+import { linkTo } from '@storybook/addon-links';
+
+// Import required styles.
+import "./docs-card.scss";
 
 // Build "card" component.
 const Card = {};
 
 // Create simple card.
-Card.Simple = ({ title, description, image, ...args }) => {
+Card.Simple = ({ title, description, image, action, theme, ...args }) => {
+	let themeClass;
+
+	switch ( theme ) {
+		case 'light':
+		case 'dark':
+			themeClass = ' csb-card--' + theme;
+			break;
+
+		default:
+			// Do nothing.
+			break;
+	}
+
+	const fig = Object.assign(
+		{
+			alt: 'Shared UI Developers Showcase',
+			src: '',
+			src2x: '',
+			width: '',
+			height: '',
+		},
+		image
+	);
+
 	return (
-		<div className="csb-card" { ...args }>
-			<div className="csb-card__image"></div>
+		<div
+			className={`csb-card csb-card--center${ !isUndefined( themeClass )
+				? themeClass
+				: ''
+			}`}
+			{ ...args }>
+			{ !isUndefined( fig.src ) && (
+				<div className="csb-card__image" aria-hidden="true">
+					<img
+						alt={ fig.alt }
+						src={ fig.src }
+						{ ... ( !isUndefined( fig.src2x ) && { srcSet: `${ fig.src } 1x, ${ fig.src2x } 2x` } ) }
+						{ ... ( !isUndefined( fig.width ) && { width: `${ fig.width }` } ) }
+						{ ... ( !isUndefined( fig.height ) && { height: `${ fig.height }` } ) }
+					/>
+				</div>
+			)}
+
 			<div className="csb-card__info">
-				{ !isUndefined(title) && <h3>{ title }</h3> }
-				{ !isUndefined(description) && <p>{ description }</p> }
+				{ !isUndefined(title) && <h3 className="csb-card__title">{ title }</h3> }
+				{ !isUndefined(description) && <p className="csb-card__description">{ description }</p> }
+				{ !isUndefined( action ) && (
+					<Button
+						label="Learn More"
+						style="secondary"
+						{ ... ( 'light' === theme && { color: 'black' } ) }
+						{ ... ( 'dark' === theme && { color: 'white' } ) }
+						className="csb-banner__cta"
+						onClick={ linkTo( action ) }
+					/>
+				)}
 			</div>
 		</div>
 	);
@@ -28,7 +83,7 @@ Card.Color = ({ title, variable, hex, hsl, ...args }) => {
 				{ !isUndefined(variable) && (
 					<div className="csb-card__row">
 						<div className="csb-card__col">
-							<h4>SCSS Variable</h4>
+							<h3>SCSS Variable</h3>
 							<p>{ variable }</p>
 						</div>
 					</div>
