a193f37b5dff6850900a22b8135adc283fb7ba6d
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-10-19 23:24:16 -0500

    🐛 fix(message): Update markup, properties and styles.

--- packages/docs-message/lib/docs-message.js
+++ packages/docs-message/lib/docs-message.js
@@ -1,32 +1,68 @@
 import React from "react";
+import { linkTo } from '@storybook/addon-links';
+import { Button } from "@wpmudev/docs-button";
+
+// Import required styles.
+import "./docs-message.scss";
 
 // Build "message" component.
-const Message = ({ status, children, ...args }) => {
-	let color;
-
-	switch ( status ) {
-		case 'success':
-			color = ' csb-message--green';
-			break;
-
-		case 'warning':
-			color = ' csb-message--yellow';
-			break;
-
-		case 'error':
-			color = ' csb-message--red';
-			break;
-
-		default:
-			// Do nothing.
-			break;
-	}
+const Message = ({ action, color, dark, children, ...args }) => {
+	const cta = Object.assign(
+		{
+			label: '',
+			icon: '',
+			style: '',
+			color: '',
+			type: '',
+			link: '',
+			page: '',
+		},
+		action
+	);
 
 	return (
-		<div className={`csb-message${ color }`} { ...args }>
-			{ children }
+		<div
+			className={`csb-message${ !isUndefined( color )
+				? ' csb-message--' + color
+				: '' }`}
+			{ ...args }>
+			<div className="csb-message__content">
+				{ children }
+			</div>
+			{ !isUndefined( cta.label ) && (
+				<div className="csb-message__action">
+					<Button
+						label={ cta.label }
+						small={ true }
+						{ ... ( !isUndefined( cta.style ) && { style: cta.style } ) }
+						{ ... ( !isUndefined( cta.color ) && { color: cta.color } ) }
+						{ ... ( !isUndefined( cta.link ) && { type: 'link' } ) }
+						{ ... ( !isUndefined( cta.link ) && { href: cta.link } ) }
+						{ ... ( !isUndefined( cta.link ) && { target: '_blank' } ) }
+						{ ... ( !isUndefined( cta.link ) && { rel: 'nofollow' } ) }
+					/>
+				</div>
+			)}
 		</div>
 	);
+}
+
+// Check if element is undefined.
+const isUndefined = (element, isNumber = false) => {
+	const isValid = 'undefined' !== typeof element;
+	const isNotEmpty = '' !== element;
+
+	if ( element && isValid && isNotEmpty ) {
+		if ( isNumber ) {
+			if ( Number.isNaN(element) ) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 // Publish required component(s).
