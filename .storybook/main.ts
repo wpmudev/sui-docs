e402561a1071d400b815588b97591570ec808ae2
Pawan Kumar <pwnkumar201@gmail.com>
2023-07-05 09:35:56 +0530

    ✨ new(global): Updated storybook to 7.0.25.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,37 +1,36 @@
 const path = require('path');
-
 module.exports = {
-	stories: [
-		"../packages/**/stories/*.stories.mdx",
-		"../packages/**/stories/*.stories.@(js|jsx|ts|tsx)",
-		"../packages/**/packages/**/stories/*.stories.mdx",
-		"../packages/**/packages/**/stories/*.stories.@(js|jsx|ts|tsx)"
-	],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-postcss",
-		"@storybook/addon-a11y"
-	],
-	webpackFinal: async (config, { configType }) => {
-		// Add SASS support.
-		config.module.rules.push({
-			test: /\.scss$/,
-			use: [
-				'style-loader',
-				'css-loader',
-				'sass-loader'
-			],
-			include: path.resolve( __dirname, '../' ),
-		});
-
-		return config;
-	},
-	framework: "@storybook/react",
-	core: {
-		"builder": "@storybook/builder-webpack5"
-	},
-	typescript: {
-		reactDocgen: 'react-docgen-typescript-plugin'
-	},
-}
+  stories: [
+    "../packages/**/stories/*.stories.mdx",
+    "../packages/**/stories/*.stories.@(js|jsx|ts|tsx)",
+    "../packages/**/packages/**/stories/*.stories.mdx",
+    "../packages/**/packages/**/stories/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-styling",
+    "@storybook/addon-a11y"
+  ],
+  webpackFinal: async (config, {
+    configType
+  }) => {
+    // Add SASS support.
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    });
+    return config;
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript-plugin'
+  },
+  docs: {
+    autodocs: true
+  }
+};
