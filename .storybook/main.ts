05333fd0cf2e05b2ab49d23636b1d70c6387d8a7
Govind Kumar <gkprmr@gmail.com>
2023-07-11 10:27:59 +0530

    refactor(storybook): add ts-loader and configure.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,40 +1,53 @@
-const path = require('path');
+const path = require("path")
+
 module.exports = {
 	stories: [
 		"../packages/**/stories/*.stories.mdx",
 		"../packages/**/stories/*.stories.@(js|jsx|ts|tsx)",
 		"../packages/**/packages/**/stories/*.stories.mdx",
-		"../packages/**/packages/**/stories/*.stories.@(js|jsx|ts|tsx)"
+		"../packages/**/packages/**/stories/*.stories.@(js|jsx|ts|tsx)",
 	],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/addon-styling",
-		"@storybook/addon-a11y"
+		"@storybook/addon-a11y",
 	],
-	webpackFinal: async (config, {
-	configType
-	}) => {
+	webpackFinal: async (config, { configType }) => {
 		// Add SASS support.
 		config.module.rules.push({
 			test: /\.scss$/,
+			use: ["style-loader", "css-loader", "sass-loader"],
+			include: path.resolve(__dirname, "../"),
+		})
+
+		// load typescript files
+		config.module.rules.push({
+			test: /\.(ts|tsx)?$/,
+			exclude: /node_modules/,
 			use: [
-				'style-loader', 
-				'css-loader', 
-				'sass-loader'
+				{
+					loader: "ts-loader",
+					options: {
+						transpileOnly: true,
+					},
+				},
 			],
-			include: path.resolve(__dirname, '../')
-		});
-		return config;
+		})
+
+		// add typescript extensions
+		config.resolve.extensions.push(".ts", ".tsx")
+
+		return config
 	},
 	framework: {
 		name: "@storybook/react-webpack5",
-		options: {}
+		options: {},
 	},
 	typescript: {
-		reactDocgen: 'react-docgen-typescript-plugin'
+		reactDocgen: "react-docgen-typescript-plugin",
 	},
 	docs: {
-		autodocs: true
-	}
-};
+		autodocs: true,
+	},
+}
