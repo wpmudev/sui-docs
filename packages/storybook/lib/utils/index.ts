e4c79c499904b58044cea4bc81574b4dd4f140bb
abdo99989 <abderrahman.douara@incsub.com>
2023-07-13 14:59:12 +0800

    Refactor: Footer Component

--- packages/storybook/lib/utils/index.ts
+++ packages/storybook/lib/utils/index.ts
@@ -2,3 +2,21 @@
 export const isEmpty = (text: string) => {
 	return "" === text
 }
+
+// Check if element is undefined.
+export const isUndefined = (element: unknown, isNumber = false) => {
+	const isValid = "undefined" !== typeof element
+	const isNotEmpty = "" !== element
+
+	if (element && isValid && isNotEmpty) {
+		if (isNumber) {
+			if (Number.isNaN(element)) {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
