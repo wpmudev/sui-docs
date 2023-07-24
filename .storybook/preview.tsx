e9e89fd45af4c059fde6ae693d1b0c2ad946091d
Govind Kumar <gkprmr@gmail.com>
2023-07-24 10:40:54 +0530

    chore: refactor folder structure and reorganize components.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import "@storybook/addon-console";
+import React, { useEffect, useState } from "react"
+import "@storybook/addon-console"
 
-import './assets/js/body-class';
-import './assets/js/html-class';
+import "./assets/js/body-class"
+import "./assets/js/html-class"
 
-import './assets/css/wordpress.css';
+import "./assets/css/wordpress.css"
 
 import "../packages/sui-css/src/scss/sui-css.scss"
 import "../packages/sui-icons/src/scss/sui-icons.scss"
@@ -17,44 +17,44 @@ const breakpoints = {
 	xl: {
 		name: 'Dell UltraSharp 24"',
 		styles: {
-			width: '1920px', // Min 1600px
-			height: '1200px',
+			width: "1920px", // Min 1600px
+			height: "1200px",
 		},
-		type: 'extra-large',
+		type: "extra-large",
 	},
 	lg: {
 		name: 'MacBook Air 13"',
 		styles: {
-			width: '1440px', // Min 1200px - Max 1599px
-			height: '900px',
+			width: "1440px", // Min 1200px - Max 1599px
+			height: "900px",
 		},
-		type: 'large',
+		type: "large",
 	},
 	md: {
-		name: 'iPad Pro 12.9-in',
+		name: "iPad Pro 12.9-in",
 		styles: {
-			width: '1024px', // Min 1024px - Max 1199px
-			height: '1366px',
+			width: "1024px", // Min 1024px - Max 1199px
+			height: "1366px",
 		},
-		type: 'medium',
+		type: "medium",
 	},
 	sm: {
-		name: 'iPad Pro 10.5-in',
+		name: "iPad Pro 10.5-in",
 		styles: {
-			width: '834px', // Min 783px - Max 1023px
-			height: '1112px',
+			width: "834px", // Min 783px - Max 1023px
+			height: "1112px",
 		},
-		type: 'small',
+		type: "small",
 	},
 	xs: {
-		name: 'iPhone 12',
+		name: "iPhone 12",
 		styles: {
-			width: '390px', // Min 0 - Max 782px
-			height: '844px',
+			width: "390px", // Min 0 - Max 782px
+			height: "844px",
 		},
-		type: 'extra-small',
+		type: "extra-small",
 	},
-};
+}
 
 export const globalTypes = {
 	theme: {
@@ -63,27 +63,28 @@ export const globalTypes = {
 		toolbar: {
 			icon: "switchalt",
 			items: [
-				{ title: "Light", left: "🌞", value: "light"},
-				{ title: "Dark", left: "🌚", value: "dark"}
-			]
-		}
+				{ title: "Light", left: "🌞", value: "light" },
+				{ title: "Dark", left: "🌚", value: "dark" },
+			],
+		},
 	},
 	direction: {
 		name: "Direction",
-		description: "The direction property specifies the text direction/writing direction within a block-level element.",
+		description:
+			"The direction property specifies the text direction/writing direction within a block-level element.",
 		toolbar: {
 			icon: "globe",
 			items: [
-				{ title: "Left to right", value: ""},
-				{ title: "Right to left", value: "rtl"}
-			]
-		}
-	}
+				{ title: "Left to right", value: "" },
+				{ title: "Right to left", value: "rtl" },
+			],
+		},
+	},
 }
 
 export const parameters = {
 	actions: {
-		argTypesRegex: "^on[A-Z].*"
+		argTypesRegex: "^on[A-Z].*",
 	},
 	controls: {
 		matchers: {
@@ -92,74 +93,54 @@ export const parameters = {
 		},
 	},
 	viewport: {
-		viewports: breakpoints
+		viewports: breakpoints,
 	},
 	backgrounds: {
-		disable: true
+		disable: true,
 	},
 	options: {
 		storySort: {
 			order: [
-				'SUI',
+				"SUI",
 				[
-					'Home',
-					'Getting Started',
+					"Home",
+					"Getting Started",
 					"What's New",
-					['Changelog', 'Roadmap'],
-					'Components',
+					["Changelog", "Roadmap"],
+					"Components",
 					[
-						'Overview',
-						'Core Elements',
+						"Overview",
+						"Core",
+						["Grid", "*"],
+						"Simple Elements",
+						["Avatar", "Button", "Icon Button", "*"],
+						"Forms",
 						[
-							'Grid',
-							'*'
+							"Form Field",
+							"Input",
+							"Text Field",
+							"Password Field",
+							"Checkbox",
+							"Radio",
+							"*",
 						],
-						'Simple Elements',
-						[
-							'Avatar',
-							'Button',
-							'Icon Button',
-							'*',
-						],
-						'Form Elements',
-						[
-							'Form Field',
-							'Input',
-							'Text Field',
-							'Password Field',
-							'Checkbox',
-							'Radio',
-							'*'
-						],
-						'Composed Elements',
-						[
-							'*'
-						],
-						'Complex Elements',
-						[
-							'*'
-						],
-						'*'
+						"Composed Elements",
+						["*"],
+						"Complex Elements",
+						["*"],
+						"*",
 					],
-					'CSS Framework',
-					[
-						'Overview',
-						'Colors',
-						'Typography',
-						'Layout',
-						'Grid',
-						'*'
-					],
-					'Icons Pack',
-					'*'
-				]
-			]
-		}
-	}
+					"CSS Framework",
+					["Overview", "Colors", "Typography", "Layout", "Grid", "*"],
+					"Icons Pack",
+					"*",
+				],
+			],
+		},
+	},
 }
 
 const WordPress = ({ children }) => {
-
 	return (
 		<div id="wpadmin">
 			<div id="adminmenumain" role="navigation" aria-label="Main Menu">
@@ -167,13 +148,24 @@ const WordPress = ({ children }) => {
 
 				<div id="adminmenuwrap">
 					<ul id="adminmenu">
-						<li className="wp-not-current-submenu wp-menu-separator" aria-hidden="true">
+						<li
+							className="wp-not-current-submenu wp-menu-separator"
+							aria-hidden="true"
+						>
 							<div className="separator"></div>
 						</li>
 
 						<li id="collapse-menu" className="hide-if-no-js">
-							<button type="button" id="collapse-button" aria-label="Collapse Main menu" aria-expanded="true">
-								<span className="collapse-button-icon" aria-hidden="true"></span>
+							<button
+								type="button"
+								id="collapse-button"
+								aria-label="Collapse Main menu"
+								aria-expanded="true"
+							>
+								<span
+									className="collapse-button-icon"
+									aria-hidden="true"
+								></span>
 								<span className="collapse-button-label">Collapse menu</span>
 							</button>
 						</li>
@@ -183,7 +175,12 @@ const WordPress = ({ children }) => {
 
 			<div id="wpcontent">
 				<div id="wpadminbar" className="nojq">
-					<div role="navigation" id="wp-toolbar" className="quicklinks" aria-label="Toolbar">
+					<div
+						role="navigation"
+						id="wp-toolbar"
+						className="quicklinks"
+						aria-label="Toolbar"
+					>
 						<ul id="wp-admin-bar-root-default" className="ab-top-menu">
 							<li id="wp-admin-bar-menu-toggle">
 								<a className="ab-item" href="#" aria-expanded="false">
@@ -196,9 +193,7 @@ const WordPress = ({ children }) => {
 				</div>
 
 				<div id="wpbody" role="main">
-					<div id="wpbody-content">
-						{ children }
-					</div>
+					<div id="wpbody-content">{children}</div>
 
 					<div className="clear"></div>
 				</div>
@@ -206,49 +201,47 @@ const WordPress = ({ children }) => {
 				<div className="clear"></div>
 			</div>
 		</div>
-	);
-};
+	)
+}
 
-const SuiWrapper = ({ children, context}) => {
+const SuiWrapper = ({ children, context }) => {
 	// get theme from global context in storybook
-	const { theme, direction } = context.globals;
+	const { theme, direction } = context.globals
 
 	// state to manage theme class
-	const [suitheme, setSuiTheme] = useState(theme || 'light');
+	const [suitheme, setSuiTheme] = useState(theme || "light")
 
 	// When the theme global changes
 	// Set the new theme
 	useEffect(() => {
-		setSuiTheme(theme);
-	}, [theme]);
+		setSuiTheme(theme)
+	}, [theme])
 
 	// Set the direction rtl/ltr
 	useEffect(() => {
 		if (direction) {
-			document.body.classList.add(direction);
-			document.documentElement.dir = direction;
+			document.body.classList.add(direction)
+			document.documentElement.dir = direction
 
 			// cleanup function to remove class added.
 			return () => {
-				document.body.classList.remove(direction);
-				document.documentElement.dir = "";
+				document.body.classList.remove(direction)
+				document.documentElement.dir = ""
 			}
 		}
-	}, [direction]);
+	}, [direction])
 
 	return (
 		<WordPress>
-			<div className={`sui-wrap sui-theme--${suitheme}`}>
-				{children}
-			</div>
+			<div className={`sui-wrap sui-theme--${suitheme}`}>{children}</div>
 		</WordPress>
-	);
+	)
 }
 
 export const decorators = [
-	( Story, context ) => (
+	(Story, context) => (
 		<SuiWrapper context={context}>
 			<Story />
 		</SuiWrapper>
-	)
-];
+	),
+]
