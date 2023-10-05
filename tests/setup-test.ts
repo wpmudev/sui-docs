76a77c74276fda8ff188a61fccb7146ce93da90d
Govind Kumar <gkprmr@gmail.com>
2023-10-05 10:57:37 +0530

    jest: add resizeObserver

--- tests/setup-test.ts
+++ tests/setup-test.ts
@@ -4,3 +4,9 @@ import { TextEncoder, TextDecoder } from "util"
 global.TextEncoder = TextEncoder
 // @ts-expect-error
 global.TextDecoder = TextDecoder
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+	observe: jest.fn(),
+	unobserve: jest.fn(),
+	disconnect: jest.fn(),
+}))
